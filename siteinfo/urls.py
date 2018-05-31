6317a7a74cfdff32010c82d6fe12ac5a84f5fd7f
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-05-31 23:28:40 +0430

    better index.html removed art page

--- siteinfo/urls.py
+++ siteinfo/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index_view, name='index'),
-    path('art/', views.art_view, name='art'),
     # # ex: /polls/5/
     # # path('<int:question_id>/', views.detail, name='detail'),
     # # ex: /polls/5/results/
--- siteinfo/views.py
+++ siteinfo/views.py
@@ -4,5 +4,3 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'siteinfo/index.html', {})
-def art_view(request):
-    return render(request, 'siteinfo/art.html', {})
