06ebefff70daa4f6320196884182919d4dc14611
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-06-08 17:28:09 +0430

    major changes

--- siteinfo/urls.py
+++ siteinfo/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     # ex: /polls/
     path('', views.index_view, name='index'),
+    re_path(r'^favicon\.ico$',RedirectView.as_view(url='/static/img/favicon.ico',  permanent=True), name='icon'),
+    re_path(r'^robots\.txt$',RedirectView.as_view(url='/static/robots.txt',  permanent=True), name='robots.txt'),
     path('trello/link', RedirectView.as_view(url='https://trello.com/invite/developingcommunity/0569c91ef09c6c05f437a75927640874', permanent=True), name='trello'),
     path('slack/link', RedirectView.as_view(url='https://join.slack.com/t/developing-community/shared_invite/enQtMzc1ODU0NTA5MzAzLTE0NmI1MmY4ZGM5NjhiODY2MDFjNmFjYjg1OWExNTBjZmViMjRkMWE4Y2U1NDk1ZjdjYzM5ODhlZmYwZWQ0MTA', permanent=True), name='slack'),
     # # ex: /polls/5/
