0946eb83020890e042be7e333d8099b2aeda2857
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-06-08 13:48:01 +0430

    changed static directory

--- web/settings.py
+++ web/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 #     #'/var/www/static/',
 # ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
