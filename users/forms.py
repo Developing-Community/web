7d6a490efcbf9630f5fb1a8c2f35e5886cf27948
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-06-09 05:27:12 +0430

    better register form

--- users/forms.py
+++ users/forms.py
@@ -4,8 +4,8 @@ from .models import MyUser
 
 
 class UserRegisterForm(forms.ModelForm):
-    email = forms.EmailField(label='Email address')
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(label='ایمیل')
+    password = forms.CharField(widget=forms.PasswordInput, label='کلمه عبور')
 
     class Meta:
         model = MyUser
@@ -16,6 +16,14 @@ class UserRegisterForm(forms.ModelForm):
             'bio',
             'learning_fields_desc',
         ]
+
+        labels = {
+            'username' : 'نام کاربری',
+            'email' : 'ایمیل',
+            'password' : 'کلمه عبور',
+            'bio' : 'معرفی',
+            'learning_fields_desc' : 'حوزه های یادگیری',
+        }
 
 
 
