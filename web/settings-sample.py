ecc2c867975058c9abd0d092834f1de0db471d24
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-06-22 13:37:16 +0430

    JWT login api - settings sample

--- web/settings-sample.py
+++ web/settings-sample.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -154,9 +154,16 @@ REST_FRAMEWORK = {
          'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         #'rest_framework.authentication.BasicAuthentication'
 
-    ), 
+    ),
     "DEFAULT_PERMISSION_CLASSES": (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
         #'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
+}
+
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': timedelta(hours=1),
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
 }
