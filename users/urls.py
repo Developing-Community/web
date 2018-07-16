e5ccfd31b1e06bf5469145e4eb0b060a4d26fff7
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-07-16 13:08:03 +0430

    user in basic django templates completed

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
