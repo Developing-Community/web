30284443980dc315f56032cf86b7cffb1215eada
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-08-12 02:25:23 +0430

    improvements in learning models

--- learning/models.py
+++ learning/models.py
@@ -16,3 +16,25 @@ class MentoringInfo(models.Model):
 class LearningInfo(models.Model):
     student = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     learning_field = models.ForeignKey(Term, on_delete=models.CASCADE, related_name='learning_field')
+
+    def __str__(self):
+        return self.mentor.username + " | " + self.mentoring_field.title
+
+class Mentoring(models.Model):
+    student_info = models.ForeignKey(LearningInfo, null= False, blank= False, related_name="student_info", on_delete=models.CASCADE)
+    mentor_info = models.ForeignKey(LearningInfo, null= False, blank= False, related_name="mentor_info", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student_info.student.username + " | " + self.mentor_info.mentor.username + " | " + self.mentor_info.mentoring_field.title
+
+
+class QualificationInfo(models.Model):
+    qualified_student_info = models.ForeignKey(LearningInfo, null=False, blank=False, related_name="qualified_student_info", on_delete=models.CASCADE)
+    qualifier = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.student.username + " | " + self.qualifier.name + " | " + self.learning_field.title + " | " + str(self.score)
+
+
+#TODO: Add model for requests - mentoring request, etc,...
