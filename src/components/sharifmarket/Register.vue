46c786fb80a44c476f3c04622c7aecf92f8a0089
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-08-29 04:05:11 +0430

    trying to get spa

--- src/components/sharifmarket/Register.vue
+++ src/components/sharifmarket/Register.vue
@@ -1,30 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 style="text-align: center; margin:30px;">ثبت نام</h1>
-                <div class="form-group">
-                    <label>نام کاربری</label>
-                    <input class="form-control" type="text" v-model="user.username">
-                </div>
-                <div class="form-group">
-                    <label>ایمیل</label>
-                    <input class="form-control" type="text" v-model="user.email">
-                </div>
-                <div class="form-group">
-                    <label>کلمه عبور</label>
-                    <input class="form-control" type="text" v-model="user.password">
-                </div>
-                <div class="form-group">
-                    <label>نام</label>
-                    <input class="form-control" type="text" v-model="user.first_name">
-                </div>
-                <div class="form-group">
-                    <label>نام خانوادگی</label>
-                    <input class="form-control" type="text" v-model="user.last_name">
-                </div>
-                <button class="btn btn-primary" @click="submit">ثبت نام</button>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h1 style="text-align: center; margin:30px;">ثبت نام</h1>
+            <div class="form-group">
+                <label>نام کاربری</label>
+                <input class="form-control" type="text" v-model="user.username">
             </div>
+            <div class="form-group">
+                <label>ایمیل</label>
+                <input class="form-control" type="text" v-model="user.email">
+            </div>
+            <div class="form-group">
+                <label>کلمه عبور</label>
+                <input class="form-control" type="text" v-model="user.password">
+            </div>
+            <div class="form-group">
+                <label>نام</label>
+                <input class="form-control" type="text" v-model="user.first_name">
+            </div>
+            <div class="form-group">
+                <label>نام خانوادگی</label>
+                <input class="form-control" type="text" v-model="user.last_name">
+            </div>
+            <button class="btn btn-primary" @click="submit">ثبت نام</button>
         </div>
     </div>
 </template>
@@ -66,9 +64,6 @@
             }
         },
         created() {
-            const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
-            };
             this.resource = this.$resource('user/register/', {}, customActions);
         }
     }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import Register from './components/sharifmarket/Register.vue';
+import GetLead from './components/sharifmarket/GetLead.vue';
 
 export const routes = [
     {
-        path: '/campaigns/sharifmarket/register', component: Register , name: 'register'
+        path: '/campaigns/sharifmarket/register',
+        component: Register,
+        name: 'register'
+    },
+    {
+        path: '/campaigns/sharifmarket',
+        component: GetLead,
+        name: 'getLead'
     }
 ];
