4ad2e77991d84d561e7022f45127f51922fabeda
spsina <snparvizi75@gmail.com>
2018-09-14 14:19:22 +0430

    basic contetnt viewset added

--- content/views.py
+++ content/views.py
@@ -7,6 +7,13 @@ from django.shortcuts import render
 from .forms import ArticleForm, ReportForm
 from .models import Content, ContentType, ContentRelation, ContentRealtionType
 
+from .serializers import ContentSerializer
+from .models import Content
+from rest_framework import viewsets
+
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
 
 def add_article(request):
     form = ArticleForm(request.POST or None, request.FILES or None)
