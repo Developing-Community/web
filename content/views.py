91ac3a891715d86d1953188f6ca5d79917a07698
Sina Parvizi <snparvizi75@gmail.com>
2018-09-17 10:44:13 +0430

    crud api views added

--- content/views.py
+++ content/views.py
@@ -7,13 +7,59 @@ from django.shortcuts import render
 from .forms import ArticleForm, ReportForm
 from .models import Content, ContentType, ContentRelation, ContentRealtionType
 
-from .serializers import ContentSerializer
+from .serializers import ContentSerializer,ContentCreateSerializer
 from .models import Content
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,RetrieveDestroyAPIView,GenericAPIView
+from taxonomy.models import Term
+from django.contrib.auth.models import AnonymousUser as NOT_LOGGED_IN
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
-class ContentViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
+class ContentListView(ListAPIView):
     serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+
+class ContentCreateView(CreateAPIView):
+    serializer_class = ContentCreateSerializer
+    def perform_create(self,serializer):
+        current_user = self.request.user
+        subject_str = serializer.validated_data['subject']
+
+        # get subject
+        try:
+            subject = Term.objects.get(title=subject_str)
+        except Term.DoesNotExist:
+            subject = Term(title=subject_str)
+            subject.save()
+        serializer.save(author=current_user,subject=subject)
+
+
+@api_view(['GET','PUT'])
+def ContentUpdateView(request,pk):
+    instance = get_object_or_404(Content,pk=pk)
+    data = ContentSerializer(instance=instance)
+    if request.method == 'PUT':
+        for item in request.data:
+            if hasattr(instance,item):
+                setattr(instance,item,request.data[item])
+        instance.save()
+        new_data = ContentSerializer(instance=instance)
+        return Response(new_data.data)
+    return Response(data.data)
+            
+
+@api_view(['GET','DELETE'])
+def ContentDelete(request,pk):
+    instance = get_object_or_404(Content,pk=pk)
+    data = ContentSerializer(instance=instance)
+    if request.method == 'DELETE':
+        instance.delete()
+        return Response({'Object Deleted'})
+    return Response(data.data)
 
 def add_article(request):
     form = ArticleForm(request.POST or None, request.FILES or None)
