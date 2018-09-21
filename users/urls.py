a2a81734c50d00e7299cdfe93b2904d059cf081c
Sina Parvizi <snparvizi75@gmail.com>
2018-09-21 14:30:22 +0430

    password reset endpoints added

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from .views import (
     UserCreateAPIView,
     ProfileUpdateAPIView,
-    ProfileRetrieveAPIView)
+    ProfileRetrieveAPIView,
+    reset_password_change)
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-
+app_name = "user"
 urlpatterns = [
     path('auth/refresh_token/', refresh_jwt_token),
     path('auth/verify_token/', verify_jwt_token),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('profile/', ProfileRetrieveAPIView.as_view(), name='view-profile'),
     path('profile/<int:id>/update/', ProfileUpdateAPIView.as_view(), name='edit-profile'),
+    path('reset-password/<key>/',reset_password_change,name="reset_password"),
 ]
