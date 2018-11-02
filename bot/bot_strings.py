1a0f71011f5e25e860f8e63f9fc60e8e6fbf3643
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-11-02 22:41:50 +0330

    fix in bot menu

--- bot/bot_strings.py
+++ bot/bot_strings.py
@@ -51,6 +51,6 @@ bot_keyboards = {
 
 def bot_profile_to_string(profile):
     profile_string = 'نام: ' + (profile.first_name or '') + ' ' + (profile.last_name or '') + '\nبیوگرافی:\n' + (profile.bio or '') + '\nمهارتها:\n'
-    profile_string += '، '.join([skill.learning_field.title for skill in profile.skills.all()])
+    profile_string += '، '.join(['#' + skill.learning_field.title for skill in profile.skills.all()])
     print(profile_string)
     return profile_string
